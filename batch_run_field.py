correct energy calculation, update visualization notebook, add model comparison notebook, update batch runs, update data with 10 iterations, add plots

batch_run_field.py
@@ -11,12 +11,11 @@ from model import IsingModel
 # input parameters
 params = {
     "L": [30],  
-    "beta": np.arange(0.1, 1.0, 0.01).tolist(),
+    "beta": np.arange(0.1, 1.0, 0.1).tolist(),
     "magnetic_field": [-1, 0, 1],
-    "hot_configuration": [False],#[True, False],
-    "algo": ["heat_bath"],#["metropolis", "heat_bath"], 
-    "activation": ["simultaneous"],
-    "seed": [69]
+    "hot_configuration": [True],
+    "algo": ["metropolis"],
+    "activation": ["simultaneous"]
 }
 
 if __name__ == "__main__":
@@ -25,7 +24,7 @@ if __name__ == "__main__":
     results = batch_run(
         IsingModel,
         parameters=params,
-        iterations=1,
+        iterations=100,
         max_steps=1000,
         number_processes=3,  
         data_collection_period=1,

ising_mod/model.py
@@ -54,7 +54,7 @@ class IsingModel(Model):
     Sets up Spin agents advances each time step.
     """
     
-    def __init__(self, L, beta, seed=69, hot_configuration=True, activation="random", algo="heat_bath", magnetic_field=0):
+    def __init__(self, L, beta, seed=None, hot_configuration=True, activation="random", algo="heat_bath", magnetic_field=0):
         """ 
         Initializes grid with Spin agents occupying each position
         """
@@ -83,8 +83,10 @@ class IsingModel(Model):
         self.beta = beta
         self.probs = [np.exp(-2.0 * self.beta * i) for i in range(2, 9, 2)]
 
-        self.seed = seed
-        np.random.seed(self.seed)
+        # set random seed if needed
+        if seed:
+            self.seed = seed
+            np.random.seed(self.seed)
 
         # cold initial configuration: 1, cold
         self.hot_configuration = hot_configuration
@@ -140,7 +142,7 @@ class IsingModel(Model):
             for neighbor in neighbors:
                 energy -= current_spin * neighbor.state
             energy -= self.magnetic_field * current_spin  # magnetic field contribution
-        return energy
+        return energy / (self.L * self.L)
 
     def calculate_magnetization(self):
         """